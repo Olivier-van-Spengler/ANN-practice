2915965abff324e60b285c6067d8c7c836c34508
Alex <reset00@live.it>
2020-11-05 11:50:58 +0100

    Dataset generator (random LTI systems) and python load mat file

---
+++ load_data.py
@@ -0,0 +1,5 @@
+from scipy.io import loadmat
+
+def load_mat(file_name):
+    annots = loadmat(file_name)
+    return annots
---
+++ matlab/DataGenerator.m
@@ -0,0 +1,33 @@
+clear all;
+clc;
+close all;
+
+n = 3;   %states
+m = 1;   %input
+p = 1;   %output
+
+N = 3;  %Number of classes/systems
+Size = 100; %Dataset size
+
+syss = {};
+T = 10; %samples per trajectory
+for i = 1:N
+    syss{end+1} = drss(n,p,m);
+end
+
+%Dataset
+label = randi(N,Size,1);
+data = {};
+for i = 1:Size
+    k = label(i);
+    sys = syss{k}; %states,output,inputs, rss conti, drss discrete (stable).
+    dt = 1;
+    Tfinal = T-1;
+    time = 0:dt:Tfinal;
+    u0 = rand(size(time,2),m);
+    U = u0;
+    x_0 = rand(n,1);
+    [Y,Td,X] = lsim(sys,U,time,x_0); %x0=[0,0]
+    data{end+1} = Y;
+end
+save('dataset.mat','data')
