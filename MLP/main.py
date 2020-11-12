5e46d4f90b40a81e64c55c9ef29f7bee57649f43
Olivier-van-Spengler <o.g.van.spengler@student.rug.nl>
2020-11-12 16:36:10 +0100

    +weight&biases
    made the dataset a tuple instead of a dictionary. Also added some code that creates the weights and biases for the model.

--- MLP/main.py
+++ MLP/main.py
@@ -3,7 +3,7 @@ import tensorflow_datasets as tfds
 import numpy as np
 from load_data import load_mat
 
-dataset = load_mat('dataset.mat')
+dataset = tuple(load_mat('dataset.mat'))
 
 # Check that dataset is a tuple
 print('dataset has type:', type(dataset))
@@ -30,4 +30,21 @@ model_weights_biases = model.get_weights()
 print('\nThere are {:,} NumPy ndarrays in our list\n'.format(len(model_weights_biases)))
 
 print(model_weights_biases)
+
+for i, layer in enumerate(model.layers):
+
+    if len(layer.get_weights()) > 0:
+        w = layer.get_weights()[0]
+        b = layer.get_weights()[1]
+
+        print('\nLayer {}: {}\n'.format(i, layer.name))
+        print('\u2022 Weights:\n', w)
+        print('\n\u2022 Biases:\n', b)
+        print('\nThis layer has a total of {:,} weights and {:,} biases'.format(w.size, b.size))
+        print('\n------------------------')
+
+    else:
+        print('\nLayer {}: {}\n'.format(i, layer.name))
+        print('This layer has no weights or biases.')
+        print('\n------------------------')
 
