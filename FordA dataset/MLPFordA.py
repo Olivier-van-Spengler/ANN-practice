5c07c28d2172f0ef919c09f0613e13e65c2e1ded
Olivier-van-Spengler <o.g.van.spengler@student.rug.nl>
2020-11-26 14:01:18 +0100

    Imported the data
    Able to import the FordA dataset, however, the algorithm suddenly fails to convert np.ndarray to tensor.

--- FordA dataset/MLPFordA.py
+++ FordA dataset/MLPFordA.py
@@ -1,19 +1,81 @@
 import tensorflow as tf
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sktime
+from sktime.utils.load_data import load_from_arff_to_dataframe
 
 # Import data
-train_data = np.loadtxt('FordA_TRAIN.txt')
-test_data = np.loadtxt('FordA_TEST.txt')
-print(len(train_data))
-print(len(test_data))
+train_x, train_y = load_from_arff_to_dataframe(
+    os.path.join(".", "FordA_TRAIN.arff")
+)
+test_x, test_y = load_from_arff_to_dataframe(
+    os.path.join(".", "FordA_TEST.arff")
+)
 
-# Separate labels
-train_labels = train_data[0:len(train_data), 0]
-test_labels = test_data[0:len(test_data), 0]
-print(test_labels)
-np.delete(train_data, 0:len(train_data), 0])
-np.delete(test_data, 0:len(test_data), 0])
-print(test_data)
+print(train_x.size)
+print(train_x.shape)
+# Convert to numpy.ndarray
+#train_x = np.ndarray(train_x)
+
+#train_x = tf.convert_to_tensor(train_x)
+train_x = pd.DataFrame(train_x).to_numpy()
+train_y = pd.DataFrame(train_y).to_numpy()
+test_x = pd.DataFrame(test_x).to_numpy()
+test_y = pd.DataFrame(test_y).to_numpy()
+
+#train_x = np.asarray(train_x).astype(np.float32)
+#train_y = np.asarray(train_y).astype(np.float32)
+#test_x = np.asarray(test_x).astype(np.float32)
+#test_y = np.asarray(test_y).astype(np.float32)
+
+print(type(train_x))
 
 # One hot encode
+
+# Create pipeline
+N = 2
+length = len(train_x)
+
+
+# Build model
+model = tf.keras.Sequential([
+             tf.keras.Input(shape=10,),
+             tf.keras.layers.Dropout(.1),
+             tf.keras.layers.Dense(500, activation = 'relu'),
+             tf.keras.layers.Dropout(.2),
+             tf.keras.layers.Dense(500, activation = 'relu'),
+             tf.keras.layers.Dropout(.2),
+             tf.keras.layers.Dense(500, activation='relu'),
+             tf.keras.layers.Dropout(.3),
+             tf.keras.layers.Dense(N, activation = 'softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+# Setting weights and biases
+model_weights_biases = model.get_weights()
+
+# Training algorithm
+epochs = 10
+batch_size = 5
+
+history = model.fit(train_x,
+                    train_y,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    validation_data=(test_x, test_y),)
+print(history)
+
+training_accuracy = history.history['accuracy']
+validation_accuracy = history.history['val_accuracy']
+
+training_loss = history.history['loss']
+validation_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
